f93601afce36a938f555dfbd84559d627e414126
Hoang <vhoang1109@gmail.com>
2026-07-14 14:50:37 +0700

    chore: "update file base.page"

--- page/base-page/base.page.ts
+++ page/base-page/base.page.ts
@@ -2,14 +2,15 @@ import { Page } from "@playwright/test";
 
 export class BasePage {
     protected page: Page;
-    protected readonly baseUrl = "https://vnexpress.net";
+    //protected readonly baseUrl = "https://vnexpress.net";
 
     constructor(page: Page) {
         this.page = page;
     }
 
     async gotoHomePage() {
-        await this.page.goto(this.baseUrl, {
+        //  await this.page.goto(this.baseUrl, {
+        await this.page.goto("/", {
             timeout: 60000,
             waitUntil: "domcontentloaded",
         });
